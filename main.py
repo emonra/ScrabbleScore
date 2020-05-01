b3764551f234196e510d620a27c64af4ab19b150
emonra <41332979+emonra@users.noreply.github.com>
2020-05-02 01:15:36 +0700

    Add score of file
    And fix grammar mistakes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def ScoreForLetter(letter):
+def ScoreOfLetter(letter):
 	letter = letter.upper()
 	score = {
 		'A': 1, 'E': 1, 'I': 1, 'L': 1, 'N': 1, 'O': 1, 'R': 1, 'S': 1, 'T': 1, 'U': 1,
@@ -12,22 +12,31 @@ def ScoreForLetter(letter):
 	return score.get(letter, 0)
 
 
-def ScoreForWord(word):
+def ScoreOfWord(word):
 	sum = 0
 	for i in word:
-		sum += ScoreForLetter(i)
+		sum += ScoreOfLetter(i)
 	return sum
 
 
-def ScoreFoxText(text):
+def ScoreOfText(text):
 	sum = 0
 	text = text.split()
 	for i in text:
-		sum += ScoreForWord(i)
+		#one letter words are not allowed
+		if len(i) > 1:
+			sum += ScoreOfWord(i)
 	return sum
 
 
+def ScoreOfFile(filepath):
+	FileRead = open(filepath, 'r')
+	return ScoreOfText(FileRead.read())
+
+
 def main():
+	filepath = #insert file path here
+	print(ScoreOfFile(filepath))
 	return 0
 
 
